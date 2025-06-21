3312799d4c9733de07c88c9700419fecd878e9d7
Mateo Tomalino <mateo.tomalino@tarmac.io>
2025-06-21 16:52:44 -0300

    fix: Remove unused videoId prop to fix Vercel build

--- src/components/HeroVideo.tsx
+++ src/components/HeroVideo.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-interface HeroVideoProps {
-  videoId?: string;
-}
+interface HeroVideoProps {}
 
-export default function HeroVideo({ videoId }: HeroVideoProps) {
+export default function HeroVideo({}: HeroVideoProps) {
   return (
     <div className="relative w-[calc(100%-2rem)] md:w-[calc(100%-4rem)] h-full mx-4 md:mx-8 overflow-hidden">
       <iframe
